a638e814297360391c3e85ca606437ab65fde6cd
Lucy Fox <lucyreneefox@gmail.com>
2016-07-22 14:42:16 -0600

    Refactored exercise 2

--- exercise_2.rb
+++ exercise_2.rb
@@ -1,8 +1,7 @@
 def get_products_of_all_ints_except_at_index(array)
-  array.each_index.map do |i|
-    temp_array = array - [array[i]]
-    temp_array.reduce(:*)
+  array.map do |num|
+    (array - [num]).reduce(:*)
   end
 end
 
-puts get_products_of_all_ints_except_at_index([1, 7, 3, 4])
+p get_products_of_all_ints_except_at_index([1, 7, 3, 4])
